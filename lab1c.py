1b84e1d37e462ee44d09e1c3a3272ef9ab433ae0
Viet Anh Dong Vu <vdong4@myseneca.ca>
2025-05-14 20:46:13 -0400

    Added lab1c

---
+++ lab1c.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+#Declaring name value
+name = 'Isaac'
+
+#Declaring integer values
+num1 = 8
+num2 = 9
+my_body_count = 0
+
+#Math
+math = num1 * num2 + my_body_count
+
+#Print command
+print(name + ' is ' + str(math) + ' years old!')
